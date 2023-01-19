69d5f34e8339f8bd8661ef38cf92fe9e92fe0b26
Ronald Chua <ronaldchua1989@gmail.com>
2023-01-19 17:33:16 +0800

    Updated changes

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.Entities
 {
     public class AppUser
-    {
-        public int Identity { get; set;    }
+
+
+    {   
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,26 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
 
-namespace API
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+});
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
